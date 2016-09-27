5ef60553dc7e28995006282773bae2753f424ff2
Michael Chadwick <michael.chadwick@gmail.com>
2016-09-27 09:30:23 -0700

    added new img gen method and option switch; added debug switch; initial image is numbered

--- lib/imgen.rb
+++ lib/imgen.rb
@@ -18,14 +18,45 @@ module Imgen
 
     # image processing
     def make_image(img, options)
+      last_pixel = {}
+      new_pixel = {}
+
       (0..img.columns).each do |x|
         (0..img.rows).each do |y|
-          red = (options[:color_dominant] == :r) ? rand(0..100) : 0
-          green = (options[:color_dominant] == :g) ? rand(0..100) : 0
-          blue = (options[:color_dominant] == :b) ? rand(0..100) : 0
-          alpha = rand(0..100)
-          img.pixel_color(x,y,"rgba(#{red}%, #{green}%, #{blue}%, #{alpha}%)")
+          case options[:method]
+          when 1
+            if !last_pixel.empty?
+              if last_pixel[:color_dominant] == 100
+                new_pixel[options[:color_dominant]] = 0
+              elsif last_pixel[:color_dominant] == 0
+                new_pixel[options[:color_dominant]] = rand(1..10) + last_pixel[options[:color_dominant]]
+              else
+                new_pixel[options[:color_dominant]] = rand(-10..10) + last_pixel[options[:color_dominant]]
+              end
+            else
+              new_pixel[:r] = (options[:color_dominant] == :r) ? rand(0..100) : 0
+              new_pixel[:g] = (options[:color_dominant] == :g) ? rand(0..100) : 0
+              new_pixel[:b] = (options[:color_dominant] == :b) ? rand(0..100) : 0
+              new_pixel[:a] = rand(0..100)
+            end
+          when 2
+            new_pixel[:r] = (options[:color_dominant] == :r) ? rand(0..100) : 0
+            new_pixel[:g] = (options[:color_dominant] == :g) ? rand(0..100) : 0
+            new_pixel[:b] = (options[:color_dominant] == :b) ? rand(0..100) : 0
+            new_pixel[:a] = rand(0..100)
+          end
+
+          img.pixel_color(x,y,"rgba(#{new_pixel[:r]}%, #{new_pixel[:g]}%, #{new_pixel[:b]}%, #{new_pixel[:a]}%)")
+          last_pixel = {r: new_pixel[:r], g: new_pixel[:g], b: new_pixel[:b], a: new_pixel[:a]}
+
+          if options[:debug]
+            print "R#{new_pixel[:r].to_s.ljust(3)}"
+            print "G#{new_pixel[:g].to_s.ljust(3)}"
+            print "B#{new_pixel[:b].to_s.ljust(3)}"
+            print "A#{new_pixel[:a].to_s.ljust(3)}| "
+          end
         end
+        print "\n" if options[:debug]
       end
 
       img_dir = options[:directory]
@@ -36,7 +67,7 @@ module Imgen
       end
 
       counter = 0
-      img_uniq = ""
+      img_uniq = "_0"
       filename_path = "#{img_dir}/#{options[:width]}x#{options[:height]}#{img_uniq}.#{img_ext}"
 
       until !File.exists?(filename_path)
